97c81a2101af614a3e59ffb30018aace0588d5ea
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2023-02-28 14:15:07 +0800

    feat(migrate): insert table emp

---
+++ migrate/migrations/000004_insert_table_emp.up.sql
@@ -0,0 +1,15 @@
+INSERT INTO emp VALUES
+(7369,'SMITH','CLERK',7902,str_to_date('17-12-1980','%d-%m-%Y %H'),800,NULL,20),
+(7499,'ALLEN','SALESMAN',7698,str_to_date('20-2-1981','%d-%m-%Y %H'),1600,300,30),
+(7521,'WARD','SALESMAN',7698,str_to_date('22-2-1981','%d-%m-%Y %H'),1250,500,30),
+(7566,'JONES','MANAGER',7839,str_to_date('2-4-1981','%d-%m-%Y %H'),2975,NULL,20),
+(7654,'MARTIN','SALESMAN',7698,str_to_date('28-9-1981','%d-%m-%Y %H'),1250,1400,30),
+(7698,'BLAKE','MANAGER',7839,str_to_date('1-5-1981','%d-%m-%Y %H'),2850,NULL,30),
+(7782,'CLARK','MANAGER',7839,str_to_date('9-6-1981','%d-%m-%Y %H'),2450,NULL,10),
+(7788,'SCOTT','ANALYST',7566,str_to_date('19-4-1987','%d-%m-%Y %H'),3000,NULL,20),
+(7839,'KING','PRESIDENT',NULL,str_to_date('17-11-1981','%d-%m-%Y %H'),5000,NULL,10),
+(7844,'TURNER','SALESMAN',7698,str_to_date('8-9-1981','%d-%m-%Y %H'),1500,0,30),
+(7876,'ADAMS','CLERK',7788,str_to_date('23-5-1987','%d-%m-%Y %H'),1100,NULL,20),
+(7900,'JAMES','CLERK',7698,str_to_date('3-12-1981','%d-%m-%Y %H'),950,NULL,30),
+(7902,'FORD','ANALYST',7566,str_to_date('3-12-1981','%d-%m-%Y %H'),3000,NULL,20),
+(7934,'MILLER','CLERK',7782,str_to_date('23-1-1982','%d-%m-%Y %H'),1300,NULL,10);
